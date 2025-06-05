a8b1cdf913c0f57caa85a734494754148caf9c5b
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2025-06-05 13:57:11 +0300

    loading and error states

--- src/app/users-server/page.tsx
+++ src/app/users-server/page.tsx
@@ -8,6 +8,7 @@ type User = {
 
 
 export default async function UsersServer() {
+	await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate a delay
 	const response = await fetch('https://jsonplaceholder.typicode.com/users');
 	const users: User[] = await response.json();
 
